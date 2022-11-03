1fa91a9661dd65e7e0096eee229ed1860b9f047b
Сергей <levanikogamer@yandex.ru>
2022-11-03 23:26:30 +0300

    Добавьте файлы проекта.

---
+++ Timer/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Timer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e){}
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(numericUpDown1.Value <= 0)
+            {
+                MessageBox.Show("Количество секунд должно быть больше 0!!");
+                return;
+            }
+            timer1.Start();
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Thread.Sleep(1000);
+            numericUpDown1.Value--;
+            if (numericUpDown1.Value == 0)
+            {
+                timer1.Stop();
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+    }
+}
